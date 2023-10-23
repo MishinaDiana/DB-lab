0eeacbf88c832eb43d3ec67c78d9ebad7e6ae8bc
stud <artemida.16.04.04@gmail.com>
2023-10-23 11:50:58 +0400

    #9 fixed

--- lr2/Task-2-9.sql
+++ lr2/Task-2-9.sql
@@ -1,4 +1,4 @@
 /*Выбор всех членов клуба, зарегестрированных
-с 12 сентября 2012 года*/
+с сентября 2012 года*/
 USE cd;
-SELECT firstname, surname, memid FROM members WHERE joindate >= '01.09.2012';
+SELECT firstname, surname, memid FROM members WHERE DATE(joindate) >= '2012-09-01';
