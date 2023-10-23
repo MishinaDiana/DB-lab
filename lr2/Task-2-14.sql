bf776d9c33eb44f109a291d7b00ceef77f746081
stud <artemida.16.04.04@gmail.com>
2023-10-23 11:38:21 +0400

    #14

--- lr2/Task-2-14.sql
+++ lr2/Task-2-14.sql
@@ -1,3 +1,3 @@
 USE cd;
-SELECT facility, 'Самый дорогой' AS 'Стоимость' FROM facilities WHERE membercost=(SELECT max(membercost) FROM facilities)
-UNION SELECT facility, 'Самый дешёвый' AS 'Стоимость'FROM facilities WHERE membercost=(SELECT min(membercost) FROM facilities);
+SELECT facility, 'Самый дорогой' AS 'Стоимость' FROM facilities WHERE initialoutlay=(SELECT max(initialoutlay) FROM facilities)
+UNION SELECT facility, 'Самый дешёвый' FROM facilities WHERE initialoutlay=(SELECT min(initialoutlay) FROM facilities);
