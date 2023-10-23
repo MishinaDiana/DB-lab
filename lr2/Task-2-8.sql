8f3882cf1410889911be0e52085d1747a6e571d4
stud <artemida.16.04.04@gmail.com>
2023-10-23 11:32:59 +0400

    #8

--- lr2/Task-2-8.sql
+++ lr2/Task-2-8.sql
@@ -3,4 +3,4 @@
  и "Дорогой" в противном случае)*/
  USE cd;
  SELECT facility, 'Дешёвый'  AS 'Качественная стоимость' FROM facilities WHERE monthlymaintenance<100
- UNION SELECT facility, 'Дорогой' AS 'Качественная стоимость' FROM facilities WHERE monthlymaintenance>=100;
+ UNION SELECT facility, 'Дорогой' FROM facilities WHERE monthlymaintenance>=100;
