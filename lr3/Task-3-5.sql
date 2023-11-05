2c4ad127b04da0dcd7bbe11053a29f92b09439d3
stud <artemida.16.04.04@gmail.com>
2023-11-05 20:47:53 +0400

    #5 fixed2

--- lr3/Task-3-5.sql
+++ lr3/Task-3-5.sql
@@ -3,4 +3,4 @@ USE cd;
 SELECT DISTINCT concat(firstname,' ', surname) as membername FROM members 
 JOIN bookings ON bookings.memid = members.memid 
 JOIN facilities ON facilities.facid = bookings.facid
-WHERE facility LIKE '%Tennis Court%';
+WHERE facility LIKE '%Court 1%' OR facility LIKE '%Court 2%';
