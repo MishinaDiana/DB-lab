ab905c02a3dd30fe1d2d2c33eced06a7bf52d260
stud <artemida.16.04.04@gmail.com>
2023-12-05 21:39:09 +0400

    fix

--- Lr7/Task-7-1.sql
+++ Lr7/Task-7-1.sql
@@ -2,8 +2,8 @@
 USE cd;
 DELIMITER //
 DROP FUNCTION IF EXISTS slot_cost//
-CREATE FUNCTION slot_cost(memid int, facid int, slots int) RETURNS int
-DETERMINISTIC
+CREATE FUNCTION slot_cost(memid int, facid int, slots int) RETURNS DECIMAL (5, 2)
+NOT DETERMINISTIC
 BEGIN 
 DECLARE cost int;
 SET cost = (SELECT IF (memid = 0, guestcost, membercost)*slots FROM facilities WHERE facid = facilities.facid);
