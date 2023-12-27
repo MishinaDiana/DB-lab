969435fd0d63f6d0626e4c00660dec741bf01a30
MishinaDiana <147314048+MishinaDiana@users.noreply.github.com>
2023-12-27 15:55:29 +0400

    .

--- Lr7/Task-7-3.sql
+++ Lr7/Task-7-3.sql
@@ -4,17 +4,17 @@ USE cd;
 
 DELIMITER //
 DROP PROCEDURE IF EXISTS payback //
-CREATE PROCEDURE payback (setdate DATE)
+CREATE PROCEDURE payback (facid INT, mon INT, ye INT)
     BEGIN
-       SELECT book.facid, fac.facility, SUM(p.payment) - fac.monthlymaintenance AS income
-       FROM bookings AS book
-	   JOIN payments AS p ON book.bookid = p.bookid
-	   JOIN facilities AS fac ON book.facid = fac.facid
-	   WHERE DATE_FORMAT(starttime, '%y %m') = DATE_FORMAT(setdate, '%y %m')
+       SELECT book.facid, fac.facility, IF(SUM(pay.payment) = fac.monthlymaintenance, 1000000000,  fac.initialoutlay/(SUM(pay.payment) - fac.monthlymaintenance)) AS income
+       FROM bookings book
+	   JOIN payments pay ON book.bookid = pay.bookid
+	   JOIN facilities fac ON book.facid = fac.facid
+	   WHERE facid = book.facid AND MONTH(book.starttime) = mon AND YEAR(book.starttime) = ye
 	   GROUP BY book.facid ORDER BY book.facid;
     END //
 
 DELIMITER ;
 
-CALL payback('2012-07-01');
+CALL payback(2, MONTH('2012-07-01'), YEAR('2012-07-01'));
 
